0b4337c4908d0e844fd9dd133a1e9ec368179520
Iqbalaulia <16410100012@stikom.edu>
2019-06-23 01:09:30 +0700

    first commit

---
+++ storage/framework/views/a3baa5e437f2b9635a65831573fcd38a165f506b.php
@@ -0,0 +1,4 @@
+<p>Hi, This is <?php echo e($data['name']); ?></p>
+<p>I have some query like <?php echo e($data['message']); ?>.</p>
+<p>It would be appriciative, if you gone through this feedback.</p>
+<?php /**PATH /home/aulia/laravel-project/riset/tutorialMailtrap/resources/views/dynamic_email_template.blade.php ENDPATH**/ ?>
